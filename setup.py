676e57d866e25815dc4486bb5d9946addc68f5fa
arthur <arthur@dd049b55-d10a-0410-9f23-e599815dd46f>
2006-11-14 18:00:05 +0000

    Bumped version number from 0.2 to 1.0rc1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import re, os
 from distutils.core import setup
 
 setup(name="honeysnap",
-      version="0.2",
+      version="1.0rc1",
       description="Honeysnap Data Analysis Framework",
       author="Jed Hale",
       author_email="jed.haile@thelogangroup.biz",
