3a4d3999cdbaf4405141ddb14e25d121929477ff
arthur <arthur@dd049b55-d10a-0410-9f23-e599815dd46f>
2007-02-24 18:12:24 +0000

    Fix another of Dave's bugs, this time in the error message

--- honeysnap/main.py
+++ honeysnap/main.py
@@ -15,7 +15,7 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program; if not, write to the Free Software
 #   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
+#                                                                                                           
 ################################################################################
 
 # $Id$
@@ -559,7 +559,7 @@ def parseOptions():
                 print "Problem with the config file! Check format and permissions"
                 sys.exit(1)
         else:
-            print "Config file %s not found!" % options['config']
+            print "Config file %s not found!" % cmdopts.config
             sys.exit(1)
 
     options = {}
